d00d51213141afc961d4aa13b3518163c0871469
Lynn Trickey <lynn.trickey@gmail.com>
2017-07-03 11:06:21 -0700

    initial commit adding these files

---
+++ VideoPlayRecord-Starter/VideoPlayRecord/RecordVideoViewController.swift
@@ -0,0 +1,31 @@
+//
+//  RecordVideoViewController.swift
+//  VideoPlayRecord
+//
+//  Created by Andy on 2/1/15.
+//  Copyright (c) 2015 Ray Wenderlich. All rights reserved.
+//
+
+import UIKit
+
+class RecordVideoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+  
+  
+  
+  @IBAction func record(_ sender: AnyObject) {
+    
+  }
+  
+  
+}
